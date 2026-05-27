ffe3dfb55660d3133251b1167ad11c33c8faa8ea
KenaiAngel <angel.sanchezroj@alumno.buap.mx>
2026-05-26 18:19:41 -0600

    Feat: Add endpoint to get all loans and for readers just his own Feat: Add endpoint to return a book and end loan Fix: Change query params in update expected return day in order to search just by loan id

--- backend/application/loans.py
+++ backend/application/loans.py
@@ -1,14 +1,31 @@
+from logging import raiseExceptions
+
 from fastapi import APIRouter, HTTPException
 from starlette import status
 from security.jwt import user_dependency
 from models.loan import LoanRequest
-from domain.loans import add_new_loan, extend_loan
+from domain.loans import add_new_loan, extend_loan, get_loan, end_loan
 from security.constants import ADMIN_ROLE, READER_ROLE
 
 router = APIRouter(
     prefix="/loans",
     tags=["loans"],
 )
+
+@router.get("",status_code=status.HTTP_200_OK)
+async def obtain_loans(user: user_dependency):
+    loans = []
+    if user['role'] == ADMIN_ROLE:
+        loans = get_loan()
+    elif user['role'] == READER_ROLE:
+        loans = get_loan(user['id'])
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Unauthorized",
+        )
+    return loans
+
 
 @router.post("", status_code=status.HTTP_201_CREATED)
 async def create_loan(loan:LoanRequest, user: user_dependency):
@@ -21,16 +38,11 @@ async def create_loan(loan:LoanRequest, user: user_dependency):
     return action
 
 @router.put("/{loan_id}",status_code=status.HTTP_202_ACCEPTED)
-async def edit_loan(loan_id:int ,user: user_dependency, user_id:int | None = None):
+async def edit_loan(loan_id:int ,user: user_dependency):
     response= {}
 
     if user['role'] == ADMIN_ROLE:
-        if user_id is None:
-            raise HTTPException(
-                status_code=status.HTTP_400_BAD_REQUEST,
-                detail="Not enough parameters",
-            )
-        response = extend_loan(loan_id,user_id)
+        response = extend_loan(loan_id)
 
     elif user['role'] == READER_ROLE:
         response = extend_loan(loan_id,user['id'])
@@ -45,6 +57,19 @@ async def edit_loan(loan_id:int ,user: user_dependency, user_id:int | None = Non
             status_code=status.HTTP_404_NOT_FOUND,
             detail=response['detail'],
         )
-
     return response['data']
+
+@router.delete("/{loan_id}",status_code=status.HTTP_204_NO_CONTENT)
+async def delete_loan(loan_id:int ,user: user_dependency):
+    if user['role'] != ADMIN_ROLE:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Forbidden",
+        )
+    loan = end_loan(loan_id)
+    print(loan)
+
+
+
+
 
--- backend/domain/loans.py
+++ backend/domain/loans.py
@@ -1,6 +1,6 @@
 from database.models import Loan, User, Book
 from models.loan import LoanRequest
-from datetime import timedelta
+from datetime import timedelta, datetime
 
 def add_new_loan(loan:LoanRequest):
     book = Book.get_or_none(Book.id == loan.book_id)
@@ -25,13 +25,84 @@ def add_new_loan(loan:LoanRequest):
     )
     return new_loan
 
-def extend_loan(loan_id:int,user_id:int):
-    loan = Loan.get_or_none((Loan.id == loan_id) & (Loan.user_id == user_id))
+def extend_loan(loan_id:int,user_id:int | None = None):
+    loan = {}
+    if user_id is None:
+        loan = Loan.get_or_none(Loan.id == loan_id)
+    else:
+        loan = Loan.get_or_none((Loan.id == loan_id) & (Loan.user_id == user_id))
     if loan is None:
         return {'status':False, 'detail':'Loan not found'}
     loan.expected_return_date += timedelta(days=10)
     loan.save(only=[Loan.expected_return_date])
     return {'status':True,'data':loan}
+
+def end_loan(loan_id:int):
+    loan = Loan.get_or_none(Loan.id == loan_id)
+    if loan is None:
+        return {'status':False, 'detail':'Loan not found'}
+    book = Book.get_or_none(Book.id == loan.book_id)
+
+    if book is None:
+        return {'status':False, 'detail':'Book not found'}
+
+    book.available_stock += 1
+    book.save(only=[Book.available_stock])
+
+    loan.is_active = False
+    loan.return_date = datetime.now()
+    loan.save(only=[Loan.is_active, Loan.return_date])
+
+    return {'status':True,'data':loan}
+
+
+def get_loan(user_id: int | None = None):
+
+    loans = []
+
+    query = (
+        Loan
+        .select(Loan, User, Book)
+        .join(User)
+        .switch(Loan)
+        .join(Book)
+        .order_by(Loan.lend_date)
+        .paginate(1, 20)
+    )
+
+    if user_id is not None:
+        query = query.where(
+            Loan.user == user_id
+        )
+
+    for loan in query:
+        current_loan = {
+            'id': loan.id,
+            'lend_date': loan.lend_date,
+            'expected_return_date': loan.expected_return_date,
+            'is_active': loan.is_active,
+            'book': {
+                'id': loan.book.id,
+                'title': loan.book.title,
+                'description': loan.book.description,
+                'available_stock': loan.book.available_stock,
+            },
+            'user': {
+                'id': loan.user.id,
+                'username': loan.user.username,
+                'email': loan.user.email,
+            },
+        }
+
+        loans.append(current_loan)
+
+    return loans
+
+
+
+
+
+
 
 
 
